5d7ee8ad1b5282bb7a9d689c9dd41f3e789a3ffe
harden <https://git.stars001.xyz:8443/harden/mj5500-plus.git>
2026-07-23 10:48:45 +0800

    day1学习

---
+++ demo/day1/vue-study/src/App.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+import Role from './components/Role.vue'
+</script>
+
+<template>
+  <Role />
+
+  <div class="box">
+    <h1>我的第一个vue项目</h1>
+  </div>
+
+  <div class="title">
+    <h1>我是一个标题</h1>
+  </div>
+  
+</template>
+
+<style scoped>
+.box {
+  width: 800px;
+  margin: 100px auto;
+  background-color: red;
+}
+.title h1{
+  font-size: 50px;
+  color: blue;
+}
+</style>
---
+++ demo/day1/vue-study/src/components/Role.vue
@@ -0,0 +1,54 @@
+<template>
+    <div>
+        <h2>角色名称：{{ name }}</h2>
+        <h2>等级：{{ level }}</h2>
+        <h2>技能：{{ skill }}</h2>
+    </div>
+
+    <div class="role-info">
+        <div class="row">
+            <span class="label">角色名称：</span>
+            <span class="value">{{ name }}</span>
+        </div>
+        <div class="row">
+            <span class="label">等级：</span>
+            <span class="value">{{ level }}</span>
+        </div>
+        <div class="row">
+            <span class="label">技能：</span>
+            <span class="value">{{ skill }}</span>
+        </div>
+    </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+const name = "战士"
+const level = ref(10)
+const skill = "剑气斩"
+</script>
+
+<style scoped>
+h2 {
+    color: red;
+}
+
+.role-info {
+    width: 400px;           /* 固定宽度，让对齐效果更明显 */
+    margin: 0 auto;         /* 整体居中 */
+}
+.row {
+    display: flex;          /* 弹性布局 */
+    justify-content: space-between;  /* 左右两端对齐 */
+    padding: 8px 0;;
+}
+.label {
+    text-align: right;      /* 标签居右 */
+    flex: 1;                /* 占一半空间 */
+    margin-right: 20px;     /* 和数值保持间距 */
+}
+.value {
+    text-align: left;       /* 数值居左 */
+    flex: 1;                /* 占一半空间 */
+}
+</style>
